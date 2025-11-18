dd11dae4166b7d7498afa5ad5c94474f7b0a7343
p9tno <plahotin_s@mail.ru>
2025-11-18 16:05:35 +0300

    add styles wp

--- functions.php
+++ functions.php
@@ -63,16 +63,16 @@ add_filter( 'mime_types', 'webp_upload_mimes' );
 
 add_action('admin_menu', 'remove_menus');
 function remove_menus() {
-    //remove_menu_page('index.php');                # Консоль 
+    // remove_menu_page('index.php');                # Консоль 
     // remove_menu_page('edit.php');                 # Записи 
     remove_menu_page('edit-comments.php');        # Комментарии 
-    //remove_menu_page('edit.php?post_type=page');  # Страницы 
-    //remove_menu_page('upload.php');               # Медиафайлы 
-    //remove_menu_page('themes.php');               # Внешний вид 
-    //remove_menu_page('plugins.php');              # Плагины 
+    // remove_menu_page('edit.php?post_type=page');  # Страницы 
+    // remove_menu_page('upload.php');               # Медиафайлы 
+    // remove_menu_page('themes.php');               # Внешний вид 
+    // remove_menu_page('plugins.php');              # Плагины 
     // remove_menu_page('users.php');                # Пользователи 
     // remove_menu_page('tools.php');                # Инструменты 
-    //remove_menu_page('options-general.php');      # Параметры 
+    // remove_menu_page('options-general.php');      # Параметры 
     // remove_menu_page('edit.php?post_type=smart-custom-fields');
 }
 
